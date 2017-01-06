febb464714dd2b1f9bc6edcae837abe06bb6716c
taka <taka@fullmarks.co.jp>
2017-01-06 17:56:00 +0900

    indent

--- app/uiflow.js
+++ app/uiflow.js
@@ -5,53 +5,53 @@ var through2 = require("through2");
 
 var api = module.exports = {};
 
-api.update = function(inputFileName, code, format) {
-    var f = flumine(function(d, ok, ng) {
+api.update = function (inputFileName, code, format) {
+  var f = flumine(function (d, ok, ng) {
 
-        var buff = [];
-        var output = through2(function(chunk, enc, cb) {
-            var svg = chunk;
-            buff.push(svg);
-            cb();
-
-        });
-        var stream = uiflow.buildWithCode(
-            inputFileName, code, format, function(error) {
-                ng(error);
-            });
-        stream.pipe(output);
-        stream.on("end", function() {
-            var buffAll = Buffer.concat(buff);
-            ok(buffAll);
-            output.end();
-        });
-
+    var buff = [];
+    var output = through2(function (chunk, enc, cb) {
+      var svg = chunk;
+      buff.push(svg);
+      cb();
 
     });
-    return f();
+    var stream = uiflow.buildWithCode(
+      inputFileName, code, format, function (error) {
+        ng(error);
+      });
+    stream.pipe(output);
+    stream.on("end", function () {
+      var buffAll = Buffer.concat(buff);
+      ok(buffAll);
+      output.end();
+    });
+
+
+  });
+  return f();
 };
 
-var stringify = function(buff) {
-    var str = String(buff);
-    return str;
+var stringify = function (buff) {
+  var str = String(buff);
+  return str;
 };
 
-var base64nize = function(buff) {
-    return buff.toString("base64");
+var base64nize = function (buff) {
+  return buff.toString("base64");
 };
-api.compile = function(code) {
-    return flumine.set({
-        svg: flumine.to(function(d) {
-            return api.update("<anon>", code, "svg");
-        }).to(stringify),
-        meta: flumine.to(function(d) {
-            return api.update("<anon>", code, "meta");
-        }).to(stringify)
-    })();
+api.compile = function (code) {
+  return flumine.set({
+    svg: flumine.to(function (d) {
+      return api.update("<anon>", code, "svg");
+    }).to(stringify),
+    meta: flumine.to(function (d) {
+      return api.update("<anon>", code, "meta");
+    }).to(stringify)
+  })();
 };
 
-api.base64png = function(code) {
-    return flumine.to(function() {
-        return api.update("<anon>", code, "png");
-    }).to(base64nize)();
+api.base64png = function (code) {
+  return flumine.to(function () {
+    return api.update("<anon>", code, "png");
+  }).to(base64nize)();
 };
--- index.js
+++ index.js
@@ -4,170 +4,170 @@ var app = electron.app;
 var BrowserWindow = electron.BrowserWindow;
 var Menu = electron.Menu;
 var dialog = electron.dialog;
-app.on('window-all-closed', function() {
-    app.quit();
+app.on('window-all-closed', function () {
+  app.quit();
 });
 
 var exec = require('child_process').exec;
 
-var warn = function(message) {
-    dialog.showMessageBox({
-        type: "warning",
-        title: "warning",
-        message: message,
-        buttons: ["close"]
-    });
+var warn = function (message) {
+  dialog.showMessageBox({
+    type: "warning",
+    title: "warning",
+    message: message,
+    buttons: ["close"]
+  });
 };
-var sendToFocusedBrowser = function(channel) {
-    return function() {
-        var win = BrowserWindow.getFocusedWindow();
-        if (!win) {
-            return warn("no selected window");
-        }
-        win.webContents.send(channel);
-    };
+var sendToFocusedBrowser = function (channel) {
+  return function () {
+    var win = BrowserWindow.getFocusedWindow();
+    if (!win) {
+      return warn("no selected window");
+    }
+    win.webContents.send(channel);
+  };
 };
 var mainMenu = {
-    label: app.getName(),
-    submenu: [{
-        label: 'About guiflow',
-        click: function() {
-            dialog.showMessageBox({
-                type: "info",
-                title: "about guiflow",
-                message: "version : 0.01",
-                buttons: ["close"]
-            });
-        }
-    }, {
-        type: 'separator'
-    }, {
-        label: 'Quit',
-        accelerator: 'CmdOrCtrl+Q',
-        click: function() {
-            app.quit();
-        }
-    }, {
-        label: 'Toggle Full Screen',
-        accelerator: 'F11',
-        click: function() {
-            var win = BrowserWindow.getFocusedWindow();
-            if (win) {
-                win.setFullScreen(!win.isFullScreen());
-            }
-        }
-    }, {
-        label: 'Toggle Dev Tool',
-        accelerator: 'F5',
-        click: function() {
-            var win = BrowserWindow.getFocusedWindow();
-            if (win) {
-                win.toggleDevTools();
-            }
-        }
-    }]
+  label: app.getName(),
+  submenu: [{
+    label: 'About guiflow',
+    click: function () {
+      dialog.showMessageBox({
+        type: "info",
+        title: "about guiflow",
+        message: "version : 0.01",
+        buttons: ["close"]
+      });
+    }
+  }, {
+    type: 'separator'
+  }, {
+    label: 'Quit',
+    accelerator: 'CmdOrCtrl+Q',
+    click: function () {
+      app.quit();
+    }
+  }, {
+    label: 'Toggle Full Screen',
+    accelerator: 'F11',
+    click: function () {
+      var win = BrowserWindow.getFocusedWindow();
+      if (win) {
+        win.setFullScreen(!win.isFullScreen());
+      }
+    }
+  }, {
+    label: 'Toggle Dev Tool',
+    accelerator: 'F5',
+    click: function () {
+      var win = BrowserWindow.getFocusedWindow();
+      if (win) {
+        win.toggleDevTools();
+      }
+    }
+  }]
 };
 var fileMenu = {
-    label: 'File',
-    submenu: [{
-        label: 'New File',
-        accelerator: 'CmdOrCtrl+N',
-        click: function() {
-            createWindow();
-        },
-    }, {
-        label: 'Open...',
-        accelerator: 'CmdOrCtrl+O',
-        click: function() {
-            dialog.showOpenDialog({
-                defaultPath: app.getPath('userDesktop'),
-                properties: ['openFile'],
-                filters: [{
-                    name: 'Documents',
-                    extensions: ['txt', 'md', 'text']
-                }, ],
-            }, function(fileNames) {
-                if (fileNames) {
-                    createWindow(fileNames[0]);
-                }
-            });
-        },
+  label: 'File',
+  submenu: [{
+    label: 'New File',
+    accelerator: 'CmdOrCtrl+N',
+    click: function () {
+      createWindow();
+    },
+  }, {
+    label: 'Open...',
+    accelerator: 'CmdOrCtrl+O',
+    click: function () {
+      dialog.showOpenDialog({
+        defaultPath: app.getPath('userDesktop'),
+        properties: ['openFile'],
+        filters: [{
+          name: 'Documents',
+          extensions: ['txt', 'md', 'text']
+        },],
+      }, function (fileNames) {
+        if (fileNames) {
+          createWindow(fileNames[0]);
+        }
+      });
+    },
 
-    }, {
-        label: "Save",
-        accelerator: 'CmdOrCtrl+S',
-        onlyFocusedWindow: true,
-        click: sendToFocusedBrowser("save"),
-    }, {
-        label: "Save As...",
-        accelerator: 'Shift+CmdOrCtrl+S',
-        click: sendToFocusedBrowser("saveAs"),
-    }]
+  }, {
+    label: "Save",
+    accelerator: 'CmdOrCtrl+S',
+    onlyFocusedWindow: true,
+    click: sendToFocusedBrowser("save"),
+  }, {
+    label: "Save As...",
+    accelerator: 'Shift+CmdOrCtrl+S',
+    click: sendToFocusedBrowser("saveAs"),
+  }]
 };
 
 var editMenu = {
-    label: 'Edit',
-    submenu: [{
-            label: "Undo",
-            accelerator: 'CmdOrCtrl+Z',
-            click: sendToFocusedBrowser("undo"),
-        }, {
-            label: "Redo",
-            accelerator: 'CmdOrCtrl+Y',
-            click: sendToFocusedBrowser("redo"),
-        }, {
-            type: 'separator'
-        }, {
-            label: "Cut",
-            accelerator: 'CmdOrCtrl+X',
-            click: sendToFocusedBrowser("cut"),
-        }, {
-            label: "Copy",
-            accelerator: 'CmdOrCtrl+C',
-            click: sendToFocusedBrowser("copy"),
-        }, {
-            label: "Paste",
-            accelerator: 'CmdOrCtrl+V',
-            click: sendToFocusedBrowser("paste"),
-        }, {
-            label: "Select All",
-            accelerator: 'CmdOrCtrl+A',
-            click: sendToFocusedBrowser("selectAll"),
-        },
+  label: 'Edit',
+  submenu: [{
+    label: "Undo",
+    accelerator: 'CmdOrCtrl+Z',
+    click: sendToFocusedBrowser("undo"),
+  }, {
+    label: "Redo",
+    accelerator: 'CmdOrCtrl+Y',
+    click: sendToFocusedBrowser("redo"),
+  }, {
+    type: 'separator'
+  }, {
+    label: "Cut",
+    accelerator: 'CmdOrCtrl+X',
+    click: sendToFocusedBrowser("cut"),
+  }, {
+    label: "Copy",
+    accelerator: 'CmdOrCtrl+C',
+    click: sendToFocusedBrowser("copy"),
+  }, {
+    label: "Paste",
+    accelerator: 'CmdOrCtrl+V',
+    click: sendToFocusedBrowser("paste"),
+  }, {
+    label: "Select All",
+    accelerator: 'CmdOrCtrl+A',
+    click: sendToFocusedBrowser("selectAll"),
+  },
 
-    ]
+  ]
 };
 
-var createWindow = function(fileName) {
-    var mainWindow = null;
-    mainWindow = new BrowserWindow({
-        width: 1100,
-        height: 800,
-        title: "guiflow -- " + (fileName ? fileName : "Untitled")
-    });
-    mainWindow.loadURL('file://' + __dirname + '/index.html');
+var createWindow = function (fileName) {
+  var mainWindow = null;
+  mainWindow = new BrowserWindow({
+    width: 1100,
+    height: 800,
+    title: "guiflow -- " + (fileName ? fileName : "Untitled")
+  });
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-    mainWindow.on('closed', function() {
-        mainWindow = null;
-    });
-    if (fileName) {
-        setTimeout(function() {
-            mainWindow.webContents.send("open", fileName);
-        }, 1000);
-    }
-    if (process.env.DEBUG) {
-        mainWindow.toggleDevTools();
-    }
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
+  if (fileName) {
+    setTimeout(function () {
+      mainWindow.webContents.send("open", fileName);
+    }, 1000);
+  }
+  if (process.env.DEBUG) {
+    mainWindow.toggleDevTools();
+  }
 };
 
-app.on('ready', function() {
-    var fileName = process.argv[2];
-    var builtMenu = Menu.buildFromTemplate([
-        mainMenu, fileMenu, editMenu
-    ]);
-    app.on("browser-window-blur", function() {});
-    app.on("browser-window-focus", function() {});
-    Menu.setApplicationMenu(builtMenu);
-    var firstWindow = createWindow(fileName);
+app.on('ready', function () {
+  var fileName = process.argv[2];
+  var builtMenu = Menu.buildFromTemplate([
+    mainMenu, fileMenu, editMenu
+  ]);
+  app.on("browser-window-blur", function () {});
+  app.on("browser-window-focus", function () {});
+  Menu.setApplicationMenu(builtMenu);
+  var firstWindow = createWindow(fileName);
 
 });
